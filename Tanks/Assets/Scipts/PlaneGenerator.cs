2322249f88c38cc02ab950bd9d734fa916c5baec
Chamath Abeysinghe <abeysinghechamath@gmail.com>
2016-11-24 12:03:07 +0530

    initial

---
+++ Tanks/Assets/Scipts/PlaneGenerator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneGenerator : MonoBehaviour {
+    public Transform[] gridPrefab;
+    public int row = 4;
+    public int col = 4;
+ 
+
+ 
+	// Use this for initialization
+	void Start () {
+        for (int r = 0; r < row; r++)
+        {
+            for (int c = 0; c < col; c++)
+            {
+                Instantiate(gridPrefab[Random.Range(0,3)], new Vector3(r*10, 0, c*10), Quaternion.identity) ;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Tanks/Assets/Scipts/TanksMovement.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TanksMovement : MonoBehaviour {
+
+    // Use this for initialization
+    int x = 0;
+    int y = 0;
+   
+
+    int currentDirection=0;
+
+    void Start () {
+        GameObject thePlayer = GameObject.Find("GameManager");
+        PlaneGenerator generatorScript = thePlayer.GetComponent<PlaneGenerator>();
+        x = (generatorScript.row-1) * 10;
+        y = (generatorScript.col-1) * 10;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x = position.x - 10;
+            if (position.x < 0) return;
+            this.transform.position = position;
+
+            int requiredDirection = 270;
+            int rightAngle = 360 - requiredDirection + currentDirection;
+            int leftAngle = requiredDirection - currentDirection;
+
+            int turnAngle = (rightAngle < leftAngle) ? rightAngle * -1 : leftAngle;
+
+            currentDirection = requiredDirection;
+
+
+
+            transform.Rotate(0, turnAngle, 0);
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x = position.x + 10;
+            if (position.x > x) return;
+            this.transform.position = position;
+
+
+            int requiredDirection = 90;
+            int rightAngle = 360 - requiredDirection + currentDirection;
+            int leftAngle = requiredDirection - currentDirection;
+
+            int turnAngle = (rightAngle < leftAngle) ? rightAngle * -1 : leftAngle;
+
+            currentDirection = requiredDirection;
+
+            transform.Rotate(0, turnAngle, 0);
+
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.z = position.z + 10;
+            if (position.z > y) return;
+            this.transform.position = position;
+
+            int requiredDirection = 0;
+            int rightAngle = 360 - requiredDirection + currentDirection;
+            int leftAngle = requiredDirection - currentDirection;
+
+            int turnAngle = (rightAngle < leftAngle) ? rightAngle * -1 : leftAngle;
+
+            currentDirection = requiredDirection;
+
+            transform.Rotate(0, turnAngle, 0);
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.z = position.z - 10;
+            if (position.z < 0) return;
+            this.transform.position = position;
+
+            int requiredDirection = 180;
+            int rightAngle = 360 - requiredDirection + currentDirection;
+            int leftAngle = requiredDirection - currentDirection;
+
+            int turnAngle = (rightAngle < leftAngle) ? rightAngle * -1 : leftAngle;
+
+            currentDirection = requiredDirection;
+
+            transform.Rotate(0, turnAngle, 0);
+        }
+    }
+    void FixedUpdate()
+    {
+
+    }
+}
